09e145974ffff93cc15d5a179e9e2fb153564200
3bdoelnaggar <3bdoelnaggar@gmail.com>
2021-05-07 18:14:57 +0200

    add loading view

--- app/src/main/java/com/elnaggar/linkdevelopmenttask/ui/newsList/NewsFragment.kt
+++ app/src/main/java/com/elnaggar/linkdevelopmenttask/ui/newsList/NewsFragment.kt
@@ -1,10 +1,13 @@
 package com.elnaggar.linkdevelopmenttask.ui.newsList
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -17,13 +20,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NewsFragment : Fragment() {
     private val viewModel: NewsViewModel by activityViewModels()
+    private lateinit var loadingView: Dialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.news_fragment, container, false)
-
         if (view is RecyclerView) {
             with(view) {
                 layoutManager = LinearLayoutManager(context)
@@ -34,12 +37,21 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadingView = createLoadingView()
+        loadingView.show()
         viewModel.loadNews()
         viewModel.newsLiveData.observe(viewLifecycleOwner) {
+            loadingView.dismiss()
             if (it != null) {
                 render(it)
             }
         }
+    }
+
+    private fun createLoadingView(): Dialog {
+        val dialog = AlertDialog.Builder(requireContext()).setView(ProgressBar(requireContext())).create()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return dialog
     }
 
     private fun render(result: Result<List<UiNews>>) {
@@ -52,7 +64,8 @@ class NewsFragment : Fragment() {
             }
 
         } else {
-            Toast.makeText(requireContext(), result.exceptionOrNull()?.message, Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), result.exceptionOrNull()?.message, Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
